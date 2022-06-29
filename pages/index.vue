c570ee37d7d88f54106c12dd593fbeca9870264a
Hubert <marshal604@gmail.com>
2022-06-29 17:49:52 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'IndexPage',
+  mounted() {
+    // this.$store.dispatch('test')
+    // this.layer1Test()
+    // this.layer2Test()
+    // this.$store.dispatch('layer3/test')
+  },
+  // methods: {
+  //   ...mapActions({
+  //     layer1Test: 'layer1/test',
+  //     layer2Test: 'layer2/test',
+  //   }),
+  // },
+}
+</script>
